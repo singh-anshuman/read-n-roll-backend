eee34ebba1296c8290eecae91be7e6dc5c9108c2
singh-anshuman <anshumansingh137@gmail.com>
2020-06-01 23:56:07 +0530

    Added h2 specific configurations

--- src/main/java/com/anshuman/readnroll/book/model/Book.java
+++ src/main/java/com/anshuman/readnroll/book/model/Book.java
@@ -1,21 +1,26 @@
 package com.anshuman.readnroll.book.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
-@EqualsAndHashCode
+@Entity
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
 	private String title;
 	private String author;
 	private String category;
