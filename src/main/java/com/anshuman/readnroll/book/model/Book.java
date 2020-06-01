2df5545d196cd85b2abb2d3024a4618d19d36552
singh-anshuman <anshumansingh137@gmail.com>
2020-06-02 00:22:54 +0530

    Modified the table name and added a few configs for h2

--- src/main/java/com/anshuman/readnroll/book/model/Book.java
+++ src/main/java/com/anshuman/readnroll/book/model/Book.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "BOOKS")
 public class Book {
 	
 	@Id
