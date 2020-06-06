11ad3a68dd55a70cba5b146c84b08cf0177d1876
singh-anshuman <anshumansingh137@gmail.com>
2020-06-06 23:21:15 +0530

    Fixed the JSON POJO mapping issue

--- src/main/java/com/anshuman/readnroll/book/model/Book.java
+++ src/main/java/com/anshuman/readnroll/book/model/Book.java
@@ -6,11 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 @Entity
 @Table(name = "BOOKS")
 public class Book {
@@ -22,5 +17,28 @@ public class Book {
 	private String title;
 	private String author;
 	private String category;
-
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
 }
