15fdf5cb0000a40873297dd8df4a9b3b3158a9c6
singh-anshuman <anshumansingh137@gmail.com>
2020-06-21 12:48:29 +0530

    Enabled CORS for the backend application

--- src/main/java/com/anshuman/readnroll/book/controller/BookController.java
+++ src/main/java/com/anshuman/readnroll/book/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,11 +21,13 @@ public class BookController {
 	BookService bookService;
 	
 	@GetMapping("/books")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public ResponseEntity<List<Book>>getBooks() {
 		return new ResponseEntity<>(bookService.getBooks(),HttpStatus.OK);
 	}
 	
 	@PostMapping("/books")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public ResponseEntity<Book> saveBook(@RequestBody Book book) {
 		return new ResponseEntity<Book>(bookService.saveBook(book),HttpStatus.CREATED);
 	}
